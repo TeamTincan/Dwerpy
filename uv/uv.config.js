c9bf0de50551393b8d6f223a992d2d2ca455587c
Team Tincan <TincanWebsites@outlook.com>
2024-05-13 18:53:52 +0000

    Update

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://interstellar-three-virid.vercel.app/o/',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: './uv/uv.handler.js',
-    bundle: './uv/uv.bundle.js',
-    config: './uv/uv.config.js',
-    sw: './uv/uv.sw.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
