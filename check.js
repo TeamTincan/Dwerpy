8d00944224cef9827d24648c080e8816540bebea
Team Tincan <TincanWebsites@outlook.com>
2024-05-14 02:57:37 +0000

    Fix

--- check.js
+++ check.js
@@ -12,21 +12,23 @@ function iframeCheck() {
     return check;
 }
 
-const inIframe = iframeCheck();
+window.addEventListener("load", (event) => {
+    const inIframe = iframeCheck();
 
-if (!inIframe) {
-    let popUp = window.open('about:blank', '_blank');
-    var noPopup;
+    if (!inIframe) {
+        let popUp = window.open('about:blank', '_blank');
+        var noPopup;
 
-    if (!popUp) {
-        var noPopup = true;
-        alert('It looks like you have Popups blocked! \nIn order for us to hide your history allow Popups. \nThank you.')
-    } else {
-        popUp.close();
-        var noPopup = false;
-        openNewTab(window.location.href);
-        if (!noPopup) {
-            window.location.href = 'https://classroom.google.com';
+        if (!popUp) {
+            var noPopup = true;
+            alert('It looks like you have Popups blocked! \nIn order for us to hide your history allow Popups. \nThank you.')
+        } else {
+            popUp.close();
+            var noPopup = false;
+            openNewTab(window.location.href);
+            if (!noPopup) {
+                window.location.href = 'https://classroom.google.com';
+            }
         }
     }
-}
+});
