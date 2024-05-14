7f5e2f2be2b2f4ad229453be67fdd78855d17f49
Rac3trase <161275083+Rac3trase@users.noreply.github.com>
2024-05-14 16:03:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -60,23 +60,32 @@ var main = document.getElementById('Main');
 if (form && input) {
     form.addEventListener('submit', async event => {
         event.preventDefault();
-        window.navigator.serviceWorker.register('./sw.js', {
-            scope: __uv$config.prefix
-        }).then(() => {
-            let url = input.value.trim();
-            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-            window.alert('If it does not work the first time click again!')
-            openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
-        });
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+        window.alert('If it does not work the first time click again!')
+        openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
     });
-}
+};
 
 function isUrl(val = '') {
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 };
+
+function iframeCheck() {
+    let check = false;
+
+    if (window.self !== window.top) {
+        check = true;
+    }
+
+    return check;
+};
+
+var inIframe = iframeCheck();
 
 var randomQuoteText = document.getElementById('randomQuoteText');
 
